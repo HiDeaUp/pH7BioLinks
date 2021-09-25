887a4c0d5442480a976733538f1d82bdc5456cd6
Pierre-Henry Soria <hi@ph7.me>
2021-09-25 17:04:08 +0930

    Handle exception from `fs.readFileSync`

--- bin/biolinks.js
+++ bin/biolinks.js
@@ -13,64 +13,63 @@ const options = {
     textAlignment: 'center',
 };
 
-const res = fs.readFileSync(path.resolve(__dirname, "data.json"));
-const userData = JSON.parse(res);
+try {
+    const res = fs.readFileSync(path.resolve(__dirname, "data.json"));
+    const userData = JSON.parse(res);
 
-if (userData) {
-    console.log(chalk.green('Cannot read data.json file!'));
+    const {
+        first_name,
+        last_name,
+        job_title,
+        github_link,
+        twitter_link,
+        linkedin_link,
+        blog_url
+    } = userData;
+
+    const data = {
+        firstName: chalk.yellowBright.underline.italic(first_name),
+        lastName: chalk.yellow.underline.italic(last_name),
+
+        labelWork: chalk.white.bold('Work:'),
+        work: chalk.magenta(job_title),
+
+        labelGitHub: chalk.white.bold('GitHub:'),
+        gitHub: chalk.cyan(github_link),
+
+        labelTwitter: chalk.white.bold('Twitter:'),
+        twitter: chalk.cyan(twitter_link),
+
+        labelLinkedIn: chalk.white.bold('LinkedIn:'),
+        linkedIn: chalk.cyan(linkedin_link),
+
+
+        labelWebsite: chalk.white.bold('Website:'),
+        website: chalk.cyan(blog_url),
+
+        labelCard: chalk.white.bold('Card:'),
+        npxCard: chalk.white.underline('npx biolinks')
+    };
+
+    const newline = '\n';
+    const output =
+        newline +
+        `${data.firstName} ${data.lastName}` +
+        newline + newline +
+        `${data.labelWork}  ${data.work}` +
+        newline +
+        `${data.labelGitHub}  ${data.gitHub}` +
+        newline +
+        `${data.labelTwitter}  ${data.twitter}` +
+        newline +
+        `${data.labelLinkedIn}  ${data.linkedIn}` +
+        newline +
+        `${data.labelWebsite}  ${data.website}` +
+        newline +
+        `${data.labelCard}  ${data.npxCard} `;
+
+    console.log(chalk.green(boxen(output, options)));
+
+} catch (err) {
+    console.log(chalk.red.bold('Cannot read data.json file!'));
 }
-
-const {
-    first_name,
-    last_name,
-    job_title,
-    github_link,
-    twitter_link,
-    linkedin_link,
-    blog_url
-} = userData;
-
-
-
-const data = {
-    firstName: chalk.yellowBright.underline.italic(first_name),
-    lastName: chalk.yellow.underline.italic(last_name),
-
-    labelWork: chalk.white.bold('Work:'),
-    work: chalk.magenta(job_title),
-
-    labelGitHub: chalk.white.bold('GitHub:'),
-    gitHub: chalk.cyan(github_link),
-
-    labelTwitter: chalk.white.bold('Twitter:'),
-    twitter: chalk.cyan(twitter_link),
-
-    labelLinkedIn: chalk.white.bold('LinkedIn:'),
-    linkedIn: chalk.cyan(linkedin_link),
-
-
-    labelWebsite: chalk.white.bold('Website:'),
-    website: chalk.cyan(blog_url),
-
-    labelCard: chalk.white.bold('Card:'),
-    npxCard: chalk.white.underline('npx biolinks')
-};
-
-const newline = '\n';
-const output =
-    newline +
-    `${data.firstName} ${data.lastName}` +
-    newline + newline +
-    `${data.labelWork}  ${data.work}` +
-    newline +
-    `${data.labelGitHub}  ${data.gitHub}` +
-    newline +
-    `${data.labelTwitter}  ${data.twitter}` +
-    newline +
-    `${data.labelLinkedIn}  ${data.linkedIn}` +
-    newline +
-    `${data.labelWebsite}  ${data.website}` +
-    newline +
-    `${data.labelCard}  ${data.npxCard} `;
-
-console.log(chalk.green(boxen(output, options)));
